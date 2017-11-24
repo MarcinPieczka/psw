17357f752bffc552bd129d8f43dabe539bd16826
pieczka.marcin <pieczka.marcin@gmail.com>
2017-11-24 15:07:43 +0100

    initial

---
+++ work-3.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hyper Text Visual Art</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="Sztuka wizualna">
+	<meta name="keywords" content="Sztuka, HTML, JS">
+	<meta name="author" content="Marcin Pieczka">
+	<link rel="stylesheet" href="base.css">
+	<link rel="stylesheet" href="neon-sign.css">
+	<script type="text/javascript" src="jquery.js"></script>
+	<script type="text/javascript" src="sprintf.js"></script>
+	<script type="text/javascript" src="utils.js"></script>
+	<script type="text/javascript" src="neon-sign.js"></script>
+</head>
+<body>
+	<div class="navbar">
+		<a href="index.html">Home</a>
+		<div class="dropdown">
+			<button class="dropbtn">Works
+				<i class="fa fa-caret-down"></i>
+			</button>
+			<div class="dropdown-content">
+				<a href="work-1.html">Work 1</a>
+				<a href="work-2.html">Work 2</a>
+				<a href="work-3.php">Work 3</a>
+			</div>
+		</div>
+	</div>
+	
+</body>
+</html>
